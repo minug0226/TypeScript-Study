b538fdea6af90f1c29b850060ac5586ec486b4bd
minug0226 <dhsfkdls04@naver.com>
2022-04-23 19:42:12 +0900

    타입 퀴즈2 완료

--- 2-types-projects-quiz/game.ts
+++ 2-types-projects-quiz/game.ts
@@ -1,12 +1,34 @@
 /**
  * Let's make a game 🕹
  */
+const position = { x: 0, y: 0 };
+type Direction = "up" | "down" | "left" | "right";
+
+function move(direction: Direction) {
+  switch (direction) {
+    case "up":
+      position.y += 1;
+      break; // 이 break를 작성하지않으면 밑에 있는것까지 다 실행이 될 것이니 작성해야한다.
+    case "down":
+      position.y -= 1;
+      break;
+    case "left":
+      position.x -= 1;
+      break;
+    case "right":
+      position.x += 1;
+      break;
+    default:
+      throw new Error(`unknown direction: ${direction}`);
+  }
+}
+
 console.log(position); // { x: 0, y: 0}
-move('up');
+move("up");
 console.log(position); // { x: 0, y: 1}
-move('down');
+move("down");
 console.log(position); // { x: 0, y: 0}
-move('left');
+move("left");
 console.log(position); // { x: -1, y: 0}
-move('right');
+move("right");
 console.log(position); // { x: 0, y: 0}
