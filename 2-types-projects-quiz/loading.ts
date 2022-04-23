b54b2a780f702bafcd7f482606ed9a3a60f107b3
minug0226 <dhsfkdls04@naver.com>
2022-04-23 19:49:16 +0900

    타입 퀴즈 3 완료 총 1,2,3은 구성은 (계산기 함수, 좌표게임, 로딩 상태표시)

--- 2-types-projects-quiz/loading.ts
+++ 2-types-projects-quiz/loading.ts
@@ -2,25 +2,43 @@
   /**
    * Print Loading State
    */
+
+  // state 라는 공통된 속성이 들어있다.
   type LoadingState = {
-    state: 'loading';
+    state: "loading";
   };
 
   type SuccessState = {
-    state: 'success';
+    state: "success";
     response: {
       body: string;
     };
   };
 
   type FailState = {
-    state: 'fail';
+    state: "fail";
     reason: string;
   };
 
-  type ResourceLoadState = LoadingState | SuccessState | FailState;
+  type ResourceLoadState = LoadingState | SuccessState | FailState; // Union 타입
 
-  printLoginState({ state: 'loading' }); // 👀 loading...
-  printLoginState({ state: 'success', response: { body: 'loaded' } }); // 😃 loaded
-  printLoginState({ state: 'fail', reason: 'no network' }); // 😱 no network
+  printLoginState({ state: "loading" }); // 👀 loading...
+  printLoginState({ state: "success", response: { body: "loaded" } }); // 😃 loaded
+  printLoginState({ state: "fail", reason: "no network" }); // 😱 no network
+
+  function printLoginState(state: ResourceLoadState) {
+    switch (state.state) {
+      case "loading":
+        console.log("loading...");
+        break;
+      case "success":
+        console.log(`😃 ${state.response.body}`); // 성공했다면 웃는 이모티콘이랑 body안에 내용을
+        break;
+      case "fail":
+        console.log(`😱 ${state.reason}`); // 실패하면 절규 이모티콘이랑 reason을
+        break;
+      default:
+        throw new Error(`unknown state: ${state}`);
+    }
+  }
 }
